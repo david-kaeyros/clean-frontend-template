12dadc7965fd30b1e0b8fba499a1eda03aa0acde
david-kaeyros <david.ouatedem@kaeyros-analytics.com>
2025-11-07 12:12:41 +0100

    clean architecture frontend template: update

--- src/modules/companies/ui/hooks/useGetCompanies.ts
+++ src/modules/companies/ui/hooks/useGetCompanies.ts
@@ -1,15 +1,9 @@
 import {companyDependencies} from "@/src/modules/companies/infra/di/container";
 import {useQuery} from "@tanstack/react-query";
-import {useEffect} from "react";
 
 export const useGetCompanies = () => {
     const getCompaniesUseCase = companyDependencies.resolve('GetCompaniesUseCase');
 
-    useEffect(() => {
-        getCompaniesUseCase.execute().then((res) => {
-            console.log(res)
-        })
-    }, [getCompaniesUseCase]);
     return useQuery({
         queryKey: ['companies'],
         queryFn: () => getCompaniesUseCase.execute(),
